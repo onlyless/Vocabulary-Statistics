28af30a7c922b67b6cfc5840e578157437d7b1ac
onlyless <13477234332@163.com>
2017-10-18 10:08:22 +0800

    first_submit

---
+++ README.md
@@ -0,0 +1,30 @@
+## 统计文本中单词频率
+<p>这个小程序可以统计文件input.txt文本中所有英文单词的词频（由高到低排序），并给出每个单词的翻译</p>
+<p>这个程序有两种方法，第一种用goole-cloud库，收费的，不过运行时间快一些，第二种模拟网页请求，时间略长,不能大量翻译.</p>
+<p>本程序用的两种方法都可以运行，第一种在本机运行后，超出了免费翻译次数，于是就用的第二种，要用第一种的话，把源代码中注释的代码取消注释即可，再把第二种的注释就可以了
+
+### 一.运行前需要安装google-cloud库,并安装Google Cloud SDK
+
+#### 1.安装相关模块
+
+``` shell
+pip install --upgrade google-cloud-translate  
+```
+
+#### 2.调用api进行翻译之前，需要安装相关证书
+
+安装  [Google Cloud SDK](https://cloud.google.com/sdk/)
+因为在本地上运行，使用的第一种方式，安装之后会有自动弹出验证邮箱的界面，如果没有在Google cloud sdk shell  运行
+``` shell
+gcloud auth application-default login
+```
+
+### 二.使用urllib取googletranslate
+模拟一个头文件，请求到translate.google.com去，然后解析出翻译文本
+
+运行方式把将要统计的文本放入Input.txt文件中，直接运行
+
+``` python
+python word.py
+```
+即可
---
+++ word.py
@@ -0,0 +1,53 @@
+#coding=utf8
+import re
+# from google.cloud import translate  
+import urllib.request
+import sys
+# translate_client = translate.Client()
+
+# language = 'zh_CN'  #将要翻译的语言
+length = 4      #提选出小于此长度的单词
+
+input = open("words.txt",'r',encoding='utf-8')
+output = open('ouput.txt','w+',encoding='utf-8')
+
+typ = sys.getfilesystemencoding()
+
+def translate(querystr, to_l="zh", from_l="en"):
+    C_agent = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"}
+    flag = 'class="t0">'
+    tarurl = "http://translate.google.com/m?hl=%s&sl=%s&q=%s \
+        " % (to_l, from_l, querystr.replace(" ", "+"))
+    request = urllib.request.Request(tarurl, headers=C_agent)
+    page = str(urllib.request.urlopen(request).read().decode(typ))
+    target = page[page.find(flag) + len(flag):]
+    target = target.split("<")[0]
+    return target
+
+try:
+    all_word = input.read()
+    l = re.split(r'[^A-Za-z]+',all_word)
+    d = {}
+    for i in l:
+        i = i.lower()
+        if d.get(i)==None:
+            d[i] = 1
+        else:
+            d[i] = d[i] +1
+    ju = 0
+    for word in sorted(d,key=d.get,reverse=True):
+        if len(word)<length:
+            continue
+
+        # translation = translate_client.translate(word,target_language=language)  
+        # tran = translation['translatedText']
+        tran = translate(word)
+        string = "%-18s%-10s%-7d\t"%(word,tran,d[word])
+        # string = "%-20s%-7d\t" %(word,d[word])
+        ju += 1
+        if ju%8==0:
+            string += '\n'
+        output.write(string)
+finally:
+    input.close()
+    output.close()
